d28af53e75dce2a502b9ddf140700a00e5d3878d
Truite Fario <matis.begot@gmail.com>
2024-10-27 20:06:40 +0100

    Imporoved responsiveness beyond 1080p

--- src/App.jsx
+++ src/App.jsx
@@ -7,91 +7,111 @@ import { Slimes } from './Slimes';
 import { Food } from './Food';
 import { slimesList } from './listeSlimes';
 import { Items } from './Items';
-import { Regions } from './Regions';
+import { Map } from './Map';
 import './assets/css/App.css';
 import { Buildings } from './Buildings';
 
 function App() {
-  document.title = "Slimepedia 2";
-  const [activeTab, setActiveTab] = useState('main');
-  const [targetTab, setTargetTab] = useState(null);
-  const [targetElement, setTargetElement] = useState(null);
-  const [darkMode, setDarkMode] = useState(() => {
-    const savedMode = localStorage.getItem('darkMode');
-    return savedMode ? JSON.parse(savedMode) : (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches);
-  });
-  const themeIcon = 'misc/' + (darkMode ? 'moon' : 'sun');
-  const [tabToRender, setTabToRender] = useState(null);
-  const tabList = useMemo(() => ['slimes', 'food', 'items', 'regions', 'weather', 'blueprints', 'buildings'], []);
-  const toggleTheme = () => { setDarkMode(!darkMode); };
+	document.title = "Slimepedia 2";
 
-  useEffect(() => {
-    localStorage.setItem('darkMode', JSON.stringify(darkMode));
-    var rootStyle = document.querySelector(':root').style;
-    rootStyle.setProperty('--menu-color-0', darkMode ? '#222' : '#EFE7D4');
-    rootStyle.setProperty('--menu-color-1', darkMode ? '#181818' : '#E9DDC7');
-    rootStyle.setProperty('--item-color', darkMode ? '#000' : '#D2B394');
-    rootStyle.setProperty('--text-color', darkMode ? '#fff' : '#000');
-    rootStyle.setProperty('--background-color', darkMode ? '#000' : '#EFE7D4');
-    document.body.style.backgroundImage = `url(${darkMode ? houseNight : houseDay})`;
-  }, [darkMode]);
+	const [activeTab, setActiveTab] = useState('main');
+	const [targetTab, setTargetTab] = useState(null);
+	const [targetElement, setTargetElement] = useState(null);
 
-  const handleTabClick = useCallback((tab, target, element) => {
-    if (tabList.includes(tab))
-      setActiveTab(tab !== activeTab ? tab : 'main');
-    else
-      setActiveTab('main');
-    setTargetTab(target ? target : null);
-    setTargetElement(element ? element : null);
-  }, [activeTab, tabList]);
+	const [darkMode, setDarkMode] = useState(() => {
+		const savedMode = localStorage.getItem('darkMode');
+		return savedMode ? JSON.parse(savedMode) : (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches);
+	});
+	const themeIcon = 'misc/' + (darkMode ? 'moon' : 'sun');
+	const [tabToRender, setTabToRender] = useState(null);
+	const tabList = useMemo(() => ['slimes', 'food', 'items', 'map', 'weather', 'blueprints', 'buildings'], []);
 
-  useEffect(() => {
-    switch (activeTab) {
-      case 'main':
-        setTabToRender(<Header dark={darkMode} />);
-        break;
-      case 'slimes':
-        setTabToRender(<Slimes slime={targetTab === null ? "pink" : targetTab} changePage={handleTabClick} tilting="left" />);
-        break;
-      case 'food':
-        setTabToRender(<Food food={targetElement === null ? "carrot" : targetElement} tab={targetTab === null ? "food" : targetTab} changePage={handleTabClick} tilting="right" />);
-        break;
-      case 'items':
-        setTabToRender(<Items item={targetElement === null ? "brine" : targetElement} tab={targetTab === null ? "resources" : targetTab} changePage={handleTabClick} tilting="left" />);
-        break;
-      case 'regions':
-        setTabToRender(<Regions tilting="left" />);
-        break;
-      case 'buildings':
-        setTabToRender(<Buildings tilting="none" />);
-        break;
-      default:
-        setTabToRender(<Header tilting="none" />);
-    }
-  }, [activeTab, targetTab, darkMode, handleTabClick, targetElement]);
+	const toggleTheme = () => { setDarkMode(!darkMode); };
+	useEffect(() => {
+		localStorage.setItem('darkMode', JSON.stringify(darkMode));
+		var rootStyle = document.querySelector(':root').style;
+		rootStyle.setProperty('--menu-color-0', darkMode ? '#222' : '#EFE7D4');
+		rootStyle.setProperty('--menu-color-1', darkMode ? '#181818' : '#E9DDC7');
+		rootStyle.setProperty('--item-color', darkMode ? '#000' : '#D2B394');
+		rootStyle.setProperty('--text-color', darkMode ? '#fff' : '#000');
+		rootStyle.setProperty('--background-color', darkMode ? '#000' : '#EFE7D4');
+		document.body.style.backgroundImage = `url(${darkMode ? houseNight : houseDay})`;
+	}, [darkMode]);
 
-  useEffect(() => {
-    const randomSlimeKey = slimesList[Object.keys(slimesList)[Math.floor(Math.random() * Object.keys(slimesList).length)]][0];
-    document.querySelector('link[rel="icon"]').href = require(`./assets/slimes/${randomSlimeKey}.png`);
-  }, []);
+	const handleTabClick = useCallback((tab, target, element) => {
+		console.log(tab, tabList.includes(tab), tabList);
+		if (tabList.includes(tab))
+			setActiveTab(tab !== activeTab ? tab : 'main');
+		else
+			setActiveTab('main');
+		setTargetTab(target ? target : null);
+		setTargetElement(element ? element : null);
+	}, [activeTab, tabList]);
 
-  return (
-    <div className="App">
-      <nav className="box-layout">
-        <NavButton name="Slimes" icon="slimes/pink" action={() => handleTabClick('slimes')} selected={activeTab === 'slimes'} tilting="left" />
-        <NavButton name="Food" icon="food/food" action={() => handleTabClick('food')} selected={activeTab === 'food'} tilting="right" />
-        <NavButton name="Items" icon="misc/res" action={() => handleTabClick('items')} selected={activeTab === 'items'} tilting="left" />
-        <NavButton name="Regions" icon="misc/world" action={() => handleTabClick('regions')} selected={activeTab === 'regions'} tilting="left" />
-        <NavButton name="Weather" icon="misc/weather" action={() => handleTabClick('weather')} selected={activeTab === 'weather'} tilting="none" />
-        <NavButton name="Blueprints" icon="misc/blueprint" action={() => handleTabClick('blueprints')} selected={activeTab === 'blueprints'} tilting="none" />
-        <NavButton name="Buildings" icon="misc/patch" action={() => handleTabClick('buildings')} selected={activeTab === 'buildings'} tilting="none" />
-        <div className="theme-btn-container">
-          <NavButton name="Switch Theme" icon={themeIcon} action={() => toggleTheme()} tilting="random" />
-        </div>
-      </nav>
-      {tabToRender}
-    </div>
-  );
+	const [wideScreen, setWideScreen] = useState(window.matchMedia("(min-width: 2560px)").matches);
+	useEffect(() => {
+		const handleResize = () => {
+			setWideScreen(window.matchMedia("(min-width: 2560px)").matches);
+		};
+
+		window.addEventListener('resize', handleResize);
+		handleResize(); // Call initially to set the state based on the current window size
+
+		return () => {
+			window.removeEventListener('resize', handleResize);
+		};
+	}, []);
+
+	var navBtnSize = wideScreen ? 125 : 100;
+
+	useEffect(() => {
+		switch (activeTab) {
+			case 'main':
+				setTabToRender(<Header dark={darkMode} />);
+				break;
+			case 'slimes':
+				setTabToRender(<Slimes slime={targetTab === null ? "pink" : targetTab} changePage={handleTabClick} tilting="left" />);
+				break;
+			case 'food':
+				setTabToRender(<Food food={targetElement === null ? "carrot" : targetElement} tab={targetTab === null ? "food" : targetTab} changePage={handleTabClick} tilting="right" />);
+				break;
+			case 'items':
+				setTabToRender(<Items item={targetElement === null ? "brine" : targetElement} tab={targetTab === null ? "resources" : targetTab} changePage={handleTabClick} tilting="left" />);
+				break;
+			case 'map':
+				setTabToRender(<Map tilting="left" />);
+				break;
+			case 'buildings':
+				setTabToRender(<Buildings tilting="none" />);
+				break;
+			default:
+				setTabToRender(<Header tilting="none" />);
+		}
+	}, [activeTab, targetTab, darkMode, handleTabClick, targetElement]);
+
+	useEffect(() => {
+		const randomSlimeKey = slimesList[Object.keys(slimesList)[Math.floor(Math.random() * Object.keys(slimesList).length)]][0];
+		document.querySelector('link[rel="icon"]').href = require(`./assets/slimes/${randomSlimeKey}.png`);
+	}, []);
+
+	return (
+		<div className="App">
+			<nav className="box-layout">
+				<NavButton name="Slimes" icon="slimes/pink" size={navBtnSize} action={() => handleTabClick('slimes')} selected={activeTab === 'slimes'} tilting="left" />
+				<NavButton name="Food" icon="food/food" size={navBtnSize} action={() => handleTabClick('food')} selected={activeTab === 'food'} tilting="right" />
+				<NavButton name="Items" icon="misc/res" size={navBtnSize} action={() => handleTabClick('items')} selected={activeTab === 'items'} tilting="left" />
+				<NavButton name="Interactive Map" icon="misc/map" size={navBtnSize} action={() => handleTabClick('map')} selected={activeTab === 'map'} tilting="none" />
+				<NavButton name="Regions" icon="misc/world" size={navBtnSize} action={() => handleTabClick('regions')} selected={activeTab === 'regions'} tilting="left" />
+				<NavButton name="Weather" icon="misc/weather" size={navBtnSize} action={() => handleTabClick('weather')} selected={activeTab === 'weather'} tilting="none" />
+				<NavButton name="Blueprints" icon="misc/blueprint" size={navBtnSize} action={() => handleTabClick('blueprints')} selected={activeTab === 'blueprints'} tilting="none" />
+				<NavButton name="Buildings" icon="misc/patch" size={navBtnSize} action={() => handleTabClick('buildings')} selected={activeTab === 'buildings'} tilting="none" />
+				<div className="theme-btn-container">
+					<NavButton name="Switch Theme" icon={themeIcon} size={navBtnSize} action={() => toggleTheme()} tilting="random" />
+				</div>
+			</nav>
+			{tabToRender}
+		</div>
+	);
 }
 
 export default App;
