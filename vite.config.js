1ba8a1ea250c0e8db703434d746b51caea970f78
Truite Fario <matis.begot@gmail.com>
2024-11-13 14:29:34 +0100

    style: réorganiser les imports dans le fichier de configuration Vite

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,8 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import process from 'process';
-
 import dotenv from 'dotenv';
+
 dotenv.config();
 
 export default defineConfig({
