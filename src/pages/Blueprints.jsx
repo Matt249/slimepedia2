a4d28dcefdebd473f174eb721528cd1dffb47c73
Truite Fario <matis.begot@gmail.com>
2024-11-19 09:42:57 +0100

    feat: ajouter de nouvelles décorations et améliorer le style des composants de blueprint

--- src/pages/Blueprints.jsx
+++ src/pages/Blueprints.jsx
@@ -14,6 +14,7 @@ import crossImg from '/src/assets/misc/cross.png';
 import trashImg from '/src/assets/misc/trash.png';
 import blueprintImg from '/src/assets/misc/blueprint.png';
 import '../css/Blueprints.css';
+import { regionInfos } from '../text/regions';
 
 const ConstructionList = ({ recipe: pattern, recipeListAdder, hideQtty = false }) => {
     const [quantity, setQuantity] = useState(1);
@@ -276,7 +277,7 @@ const DecorationsPage = ({ recipeListAdder, blueprint = null }) => {
                     </NavLink>
                 ))}
             </div>
-            <div className='blueprint-infos'>
+            <div className='blueprint-infos decoration'>
                 <div className='vac-upgrade-title-box'>{console.log(blueprint)}
                     <img src={blueprint === null ? blueprintImg : mediaFetcher(`deco/${blueprint}.png`)} alt={blueprint === null ? '' : decorationsList[blueprint][0]} />
                     <h1>{blueprint === null ? 'Select a blueprint' : decorationsList[blueprint][0]}</h1>
@@ -295,6 +296,15 @@ const DecorationsPage = ({ recipeListAdder, blueprint = null }) => {
                         </>
                     )}
                 </div>
+                <div className='decoration-theme'>
+                    <h2>Region theme</h2>
+                    {blueprint !== null && (
+                        <>
+                            <img src={mediaFetcher(`world/${decorationsList[blueprint][3]}.png`)} alt={decorationsList[blueprint][0]} />
+                            <p>{regionInfos[decorationsList[blueprint][3]][0]}</p>
+                        </>
+                    )}
+                    </div>
             </div>
         </>
     );
