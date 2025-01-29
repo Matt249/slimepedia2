66441f84287ae60c2bd3fdd67617812c82f99fc5
= <matis.begot@gmail.com>
2025-01-29 18:05:13 +0100

    feat: amélioration du style de la page Weather avec ajout d'une image de fond et ajustements CSS wip

--- src/pages/Weather.tsx
+++ src/pages/Weather.tsx
@@ -4,19 +4,23 @@ import { weatherList, weatherNames } from "../text/weather";
 import { mediaFetcher } from "../media-manager";
 
 export const Weather: React.FC = () => {
+    const backgroundImage = {
+        backgroundImage: `url(${mediaFetcher('wait/archway.jpg')})`,
+    }
     return (
         <div>
-            <div className="weather-list">
+            <div className="weather-list" x>
                 {weatherNames.map((weather: string) => (
                     <div
                         key={weather}
                         className="weather-element"
+                        style={backgroundImage}
                     >
                         <img src={mediaFetcher(`world/${weather}.png`)} />
                         <h1>{weatherList[weather][0]}</h1>
-                        <video autoPlay loop muted className={`video-${weather}`}>
+                        {/* <video autoPlay loop muted className={`video-${weather}`}>
                             <source src={mediaFetcher(`videos/ps.webm`)} type="video/webm" />
-                        </video>
+                        </video> */}
                     </div>
                 ))
                 }
