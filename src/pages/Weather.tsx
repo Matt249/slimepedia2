b307be7f30c3937615470d20cdb30a8d473e7026
TroutMaster <matis.begot@gmail.com>
2025-03-07 17:03:10 +0100

    feat: ajout de la section d'informations météorologiques avec vidéo et style amélioré

--- src/pages/Weather.tsx
+++ src/pages/Weather.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import '../css/Weather.css';
 import { weatherList, weatherNames } from "../text/weather";
 import { mediaFetcher } from "../media-manager";
+import { Biomes } from "../components/Biomes";
 
 export const Weather: React.FC = () => {
     return (
@@ -25,7 +26,18 @@ export const Weather: React.FC = () => {
                 ))
                 }
             </div>
-            <div className="weather-info">weather infos</div>
+            <div className="weather-info">
+                <video className="video-weather" src={mediaFetcher("videos/ra.webm")} autoPlay loop muted />
+                <div className="weather-info-title">
+                    <img src={mediaFetcher("world/rain.png")} />
+                    <h1>Weather</h1>
+                    <h2>Hold on to your plorts!</h2>
+                    <p>
+                        Even by the standards of Rainbow Island, slimefall is an unusual sight, with dozens of slimes raining down from the clouds above. As with many things, the slimes don&apos;t seem to be particularly bothered by it all, but a rancher would do well to remain cautious. With so many new slimes dropped into an area at once, slimefall can easily lead to Tarr outbreaks.
+                    </p>
+                </div>
+                <div className="weather-info-content"><Biomes spawnList={['co', 'rf', 'ss', 'ev', 'pb', 'gl']} /></div>
+            </div>
         </div>
     );
 }
